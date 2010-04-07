da5a8c3d6a26db81cd3c810e6c99b495c0391bd5
Nelson Morris <nmorris@nelsonmorris.net>
2010-04-07 18:37:58 -0500

    Upgrade to use HoustonBookSearch 0.1.0

--- list.rb
+++ list.rb
@@ -1,15 +1,5 @@
-begin
-  # Try to require the preresolved locked set of gems.
-  require File.expand_path('../.bundle/environment', __FILE__)
-rescue LoadError
-  # Fall back on doing an unlocked resolve at runtime.
-  require "rubygems"
-  require "bundler"
-  Bundler.setup
-end
-
-require 'HoustonLibrarySearch'
-require 'scrape.rb'
+require "HoustonLibrarySearch"
+require 'wishlist.rb'
 
 if ARGV.length != 1
   puts "This script requires one argument, the wishlist id."
@@ -19,9 +9,12 @@ else
   w.list.each do |a|
     if a[1]
       puts "#{a[0]}"
-      HoustonLibrarySearch.search(a[1]).each do |library, listings|
-        puts "  #{library}:"
-        listings.each {|l| puts "    #{l.location} #{l.call_number} #{l.status} #{l.due_date}"}
+      parser = HoustonLibrarySearch::ISBN.new
+			parser.search(a[1]).each do |library, listings|
+				unless listings.empty?
+        	puts "  #{library}:"
+        	listings.each {|l| puts "    #{l.location} #{l.call_number} #{l.status} #{l.due_date}"}
+				end
       end
       puts
     end
